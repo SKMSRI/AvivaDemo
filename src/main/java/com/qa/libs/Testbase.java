e7efe577e914c9694811608e6ffbfbbf6565f29a
SAURABH KUMAR SRIVASTAVA <78096853+SaurabhKumarSrivastava24@users.noreply.github.com>
2021-09-16 05:35:55 +0530

    Added

--- src/main/java/com/qa/libs/Testbase.java
+++ src/main/java/com/qa/libs/Testbase.java
@@ -1,10 +1,33 @@
 package com.qa.libs;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Testbase {
-
+	public static WebDriver driver;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	
+	System.setProperty("webdriver.chrome.driver",
+			"C:\\SAURABH\\Workspace\\SeleniumPracticeSession\\Driver\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();// lauchchrome
+	driver.manage().window().maximize();
+	driver.manage().deleteAllCookies();
+	driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+	driver.get("https://www.avivaindia.com");
+	String title = driver.getTitle();// Get Title
+	System.out.println(title);
+	// Validation point
+	if (title.equals("Life Insurance Company | Best Life Insurance Plans India - Aviva Life Insurance")) {
+		System.out.println("correct Title");
+	} else {
+		System.out.println("Incorrect title");
 	}
+	System.out.println(driver.getCurrentUrl());// for print current url
+	//driver.quit();// for quit bowser
 
+}
+	
 }
--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -1,5 +1,36 @@
 package com.qa.pages;
 
-public class HomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.qa.libs.Testbase;
+
+public class HomePage extends Testbase {
+
+	public static void main(String[] args) {
+
+		@FindBy(className= "About Us")
+		@CacheLookup // is used to improve performance
+		WebElement About Us;
+
+		@FindBy(xpath= "//a[normalize-space()='Aviva Edge']")
+		WebElement Aviva Edge;
+		
+		// Initializing the Page Objects:
+		public HomePage() {
+			PageFactory.initElements(driver, this);
+		}	
+		//Actions--validations
+		public String validatepagetitle(){
+			return driver.pagetitle();
+		}
+		
+		public boolean ValidateAvivalogo(){
+			return Aviva Edge.isDisplayed();
+		}
+		}
 }
+
+
